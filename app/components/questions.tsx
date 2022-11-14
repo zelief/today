fcf897f1bb0a175f5ff21be179c54dccced1d984
ziya <ziyaelarief@gmail.com>
2022-11-14 13:38:56 +0700

    add back function

--- app/components/questions.tsx
+++ app/components/questions.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import { Question } from '@prisma/client';
-import { useState } from 'react';
+import {
+  ButtonHTMLAttributes,
+  HTMLAttributes,
+  MouseEventHandler,
+  useState,
+} from 'react';
 
 export function Questions({ questions }: { questions: Question[] }) {
   const [questionIdx, setQuestionIdx] = useState(0);
@@ -37,24 +42,65 @@ export function Questions({ questions }: { questions: Question[] }) {
           <strong>{questions[questionIdx].question}</strong>?
         </h5>
         <div className="flex w-full mt-4 space-x-3 md:mt-6">
-          <button
+          <AnswerButton
             onClick={() => answer(true)}
-            className="w-full py-2 items-center text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            isActive={answers[questionIdx]}
           >
             Ya
-          </button>
-          <button
+          </AnswerButton>
+          <AnswerButton
             onClick={() => answer(false)}
-            className="w-full py-2 items-center text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
+            isActive={answers[questionIdx] === false}
           >
             Tidak
-          </button>
+          </AnswerButton>
         </div>
       </div>
 
-      <div className="text-right">
-        {questionIdx + 1}/{questions.length}
+      <div className="flex justify-between items-center">
+        {questionIdx == 0 ? (
+          <div></div>
+        ) : (
+          <button onClick={() => setQuestionIdx(questionIdx - 1)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              className="text-indigo-600 hover:text-indigo-800 active:text-indigo-800"
+            >
+              <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+            </svg>
+          </button>
+        )}
+        <div>
+          {questionIdx + 1}/{questions.length}
+        </div>
       </div>
     </>
   );
 }
+
+const AnswerButton: React.FC<
+  { isActive: boolean } & ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ isActive, onClick, children }) => {
+  if (isActive) {
+    return (
+      <button
+        onClick={onClick}
+        className="w-full py-2 items-center text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        {children}
+      </button>
+    );
+  }
+  return (
+    <button
+      onClick={onClick}
+      className="w-full py-2 items-center text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
+    >
+      {children}
+    </button>
+  );
+};
