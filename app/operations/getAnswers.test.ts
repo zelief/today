17dde1889326438a7633919cb0fec936efdd5f2c
ziya <ziyaelarief@gmail.com>
2022-11-19 08:49:01 +0700

    create and pass test it should not create new result if the result of current date is exist

--- app/operations/getAnswers.test.ts
+++ app/operations/getAnswers.test.ts
@@ -41,4 +41,65 @@ describe('Get answers operation', () => {
   it('should throw error if questions is empty', async () => {
     await expect(getAnswers()).rejects.toThrow('Question not found');
   });
+
+  it('should not create new result if the result of current date is exist', async () => {
+    await prisma.question.createMany({
+      data: [
+        {
+          id: 99,
+          question: 'question 1',
+          value: 4,
+        },
+        {
+          id: 100,
+          question: 'question 2',
+          value: 4,
+        },
+        {
+          id: 101,
+          question: 'question 3',
+          value: 4,
+        },
+      ],
+    });
+
+    await prisma.result.create({
+      data: {
+        score: 0,
+        answers: {
+          createMany: {
+            data: [
+              {
+                questionId: 99,
+                yes: false,
+              },
+              {
+                questionId: 100,
+                yes: false,
+              },
+              {
+                questionId: 101,
+                yes: false,
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    await getAnswers();
+
+    const result = await prisma.result.findMany();
+    const answers = await prisma.answer.findMany();
+    const questions = await prisma.question.findMany();
+
+    expect(result).toHaveLength(1);
+    expect(answers).toHaveLength(3);
+    expect(answers[0].resultId).toBe(result[0].id);
+    expect(answers[1].resultId).toBe(result[0].id);
+    expect(answers[2].resultId).toBe(result[0].id);
+    expect(answers[0].questionId).toBe(questions[0].id);
+    expect(answers[1].questionId).toBe(questions[1].id);
+    expect(answers[2].questionId).toBe(questions[2].id);
+  });
 });
