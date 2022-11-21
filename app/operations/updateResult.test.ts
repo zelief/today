8db2550289f62b1c12a7377d7aee5b872b4a5319
ziya <ziyaelarief@gmail.com>
2022-11-21 14:14:50 +0700

    add test it should save the score and answers correctly

--- app/operations/updateResult.test.ts
+++ app/operations/updateResult.test.ts
@@ -1,30 +1,35 @@
 import { prisma } from '@app/db';
 
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it, beforeEach } from '@jest/globals';
+
 import { updateResult } from './updateResult';
 
 describe('Update the result and the answers', () => {
-  it('should error if invlaid answer id is found', async () => {
-    await prisma.question.createMany({
-      data: [
-        {
-          id: 99,
-          question: 'question 1',
-          value: 4,
-        },
-        {
-          id: 100,
-          question: 'question 2',
-          value: 4,
-        },
-        {
-          id: 101,
-          question: 'question 3',
-          value: 4,
-        },
-      ],
-    });
+  const questions = [
+    {
+      id: 99,
+      question: 'question 1',
+      value: 3,
+    },
+    {
+      id: 100,
+      question: 'question 2',
+      value: 2,
+    },
+    {
+      id: 101,
+      question: 'question 3',
+      value: 5,
+    },
+  ];
 
+  beforeEach(async () => {
+    await prisma.question.createMany({
+      data: questions,
+    });
+  });
+
+  it('should error if invlaid answer id is found', async () => {
     const result = await prisma.result.create({
       data: {
         score: 0,
@@ -33,17 +38,17 @@ describe('Update the result and the answers', () => {
             data: [
               {
                 id: 101,
-                questionId: 99,
+                questionId: questions[0].id,
                 yes: false,
               },
               {
                 id: 102,
-                questionId: 100,
+                questionId: questions[1].id,
                 yes: false,
               },
               {
                 id: 103,
-                questionId: 101,
+                questionId: questions[2].id,
                 yes: false,
               },
             ],
@@ -66,26 +71,6 @@ describe('Update the result and the answers', () => {
     );
   });
   it('should error if result is not found', async () => {
-    await prisma.question.createMany({
-      data: [
-        {
-          id: 99,
-          question: 'question 1',
-          value: 4,
-        },
-        {
-          id: 100,
-          question: 'question 2',
-          value: 4,
-        },
-        {
-          id: 101,
-          question: 'question 3',
-          value: 4,
-        },
-      ],
-    });
-
     const newResult = {
       id: 88,
       answers: [
@@ -98,5 +83,55 @@ describe('Update the result and the answers', () => {
     await expect(updateResult(newResult)).rejects.toThrow(
       'Result is not found'
     );
+  });
+
+  it.only('should save the score and answers correctly', async () => {
+    const result = await prisma.result.create({
+      data: {
+        score: 0,
+        answers: {
+          createMany: {
+            data: [
+              {
+                id: 101,
+                questionId: questions[0].id,
+                yes: false,
+              },
+              {
+                id: 102,
+                questionId: questions[1].id,
+                yes: false,
+              },
+              {
+                id: 103,
+                questionId: questions[2].id,
+                yes: false,
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    const newResult = {
+      id: result.id,
+      answers: [
+        { id: 101, yes: true },
+        { id: 102, yes: true },
+        { id: 103, yes: false },
+      ],
+    };
+
+    await updateResult(newResult);
+
+    const resultAfter = await prisma.result.findUnique({
+      where: { id: newResult.id },
+      include: { answers: { select: { id: true, yes: true } } },
+    });
+
+    expect(resultAfter?.score).toBe(50); // score = sum of the yes answers' question's value divided by sum of questions' value in percent
+    expect(resultAfter?.answers[0].yes).toBe(true);
+    expect(resultAfter?.answers[0].yes).toBe(true);
+    expect(resultAfter?.answers[0].yes).toBe(false);
   });
 });
