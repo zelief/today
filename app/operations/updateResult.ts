b9430d9507fca36c9f6e4641c4fb24417be69b79
ziya <ziyaelarief@gmail.com>
2022-12-04 08:13:00 +0700

    pass the test

--- app/operations/updateResult.ts
+++ app/operations/updateResult.ts
@@ -11,18 +11,36 @@ type NewResult = {
 export async function updateResult(newResult: NewResult) {
   const result = await prisma.result.findUnique({
     where: { id: newResult.id },
-    include: { answers: true },
+    include: { answers: { include: { question: true } } },
   });
 
   if (result == null) {
     throw new Error('Result is not found');
   }
 
-  const validAnswerIds = result?.answers.map((answer) => answer.id);
+  let score = 0;
+  const totalValues = result.answers.reduce((accumulator, obj) => {
+    return accumulator + obj.question.value;
+  }, 0);
 
-  newResult.answers.forEach((answer) => {
-    if (!validAnswerIds.includes(answer.id)) {
+  for await (const newAnswer of newResult.answers) {
+    const oldAnswer = result.answers.find(
+      (answer) => answer.id == newAnswer.id
+    );
+
+    if (!oldAnswer) {
+      console.log('masuk');
       throw new Error('Answer id is invalid');
     }
-  });
+
+    if (newAnswer.yes) {
+      score += Math.round((oldAnswer.question.value / totalValues) * 100);
+      await prisma.answer.update({
+        where: { id: newAnswer.id },
+        data: { yes: true },
+      });
+    }
+  }
+
+  await prisma.result.update({ where: { id: newResult.id }, data: { score } });
 }
