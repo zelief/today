e3520232811d89aca5639f89342eedd3fec1ed75
ziya <ziyaelarief@gmail.com>
2023-01-02 10:32:24 +0700

    fix test inconsistencies

--- jest/setupTeardown.ts
+++ jest/setupTeardown.ts
@@ -1,9 +1,13 @@
 import { prisma } from '@app/db';
-import { beforeEach } from '@jest/globals';
+import { beforeEach, afterAll } from '@jest/globals';
 
 beforeEach(async () => {
   const deleteAnswers = prisma.answer.deleteMany();
   const deleteResults = prisma.result.deleteMany();
   const deleteQuestions = prisma.question.deleteMany();
-  return await prisma.$transaction([deleteAnswers, deleteResults, deleteQuestions]);
+  await prisma.$transaction([deleteAnswers, deleteResults, deleteQuestions]);
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
